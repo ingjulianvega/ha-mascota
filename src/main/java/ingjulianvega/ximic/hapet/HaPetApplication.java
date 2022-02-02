e25fdab7c9223f677be05ee87ffbb5bc0ae14da7
Julian Camilo Vega Rocha <julian.vega@globant.com>
2022-02-01 21:49:54 -0500

    ⚡️ Initial commit POM basic dependencies

---
+++ src/main/java/ingjulianvega/ximic/hapet/HaPetApplication.java
@@ -0,0 +1,13 @@
+package ingjulianvega.ximic.hapet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ingjulianvega/ximic/hapet/HaPetApplicationTests.java
@@ -0,0 +1,13 @@
+package ingjulianvega.ximic.hapet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
